4d627a39c7bacbb8d46de2dc235ff95e8bd1c116
Pork <180156791+Porklings@users.noreply.github.com>
2026-04-23 11:29:08 +1000

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyAqjRitDRmwSDcBay1UWdwu4Cqh_7Sw0xA",
+  authDomain:        "yesnoteslol.firebaseapp.com",
+  projectId:         "yesnoteslol",
+  storageBucket:     "yesnoteslol.firebasestorage.app",
+  messagingSenderId: "266554154996",
+  appId:             "1:266554154996:web:472604ee024391c08b63a1"
+};
